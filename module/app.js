5ce705c5bc5e182e1bc0c05c559421649da606c5
szczepaniakmichal <mssk87@gmail.com>
2020-03-17 23:28:05 +0100

    module + require

---
+++ module/app.js
@@ -0,0 +1,6 @@
+// console.log('jestem w app');
+const notes = require('./notes');
+// console.log(notes);
+//
+// const mod = require('module');
+// console.log(mod.wrapper);
---
+++ module/notes.js
@@ -0,0 +1,7 @@
+
+console.log('jestem w notes');
+console.log('dirname', __dirname);
+console.log('filename', __filename);
+console.log('module', module);
+
+module.exports = 'coś zwrócone z notes';
