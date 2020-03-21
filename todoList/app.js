a68dc546fe6d9b9dda02801f2025e23a04799c52
szczepaniakmichal <mssk87@gmail.com>
2020-03-21 01:28:44 +0100

    get data from file / add module fileSystem / function handleData(type, title)

--- todoList/app.js
+++ todoList/app.js
@@ -1,27 +1,41 @@
 const parseArgs = require('minimist');
 const colors = require('colors');
+const fs = require('fs');
 
-const command = parseArgs(process.argv.slice(2,3));
+const command = parseArgs(process.argv.slice(2, 3));
 delete command._
 console.log(command);
 
 const handleCommand = ({add, remove, list}) => {
     if (add) {
-        if (typeof add!== "string") {
+        if (typeof add !== "string") {
             return console.log("Wprowadzana wartosc nie jest textem".red);
         } else if (add.length < 7) {
             return console.log('wprowadz text dluzszy niz 7 znakow'.red);
         }
+        handleDate(1, add);
     } else if (remove) {
-        if (typeof remove !== "string" || remove.length < 7 ) {
-            return console.log('wpisz nazwe zadania które jest textem i ma minimum 7 znaków');
+        if (typeof remove !== "string" || remove.length < 7) {
+            return console.log('wpisz nazwe zadania które jest textem i ma minimum 7 znaków'.red);
         }
-        console.log('bede cos usuwac');
+        handleDate(2, remove);
     } else if (list || list === '') {
-        console.log('pokaze liste');
+        handleDate(3, null);
     } else {
-        console.log('polecenie nie jest poprawne. Uzyj --add="nazwa zadania", --remove="nazwa zadania", --list');
+        console.log('polecenie nie jest poprawne. Uzyj --add="nazwa zadania", --remove="nazwa zadania", --list'.red);
     }
+};
+
+const handleDate = (type, title) => {
+    // type - number ( 1 - add; 2 - remove; 3 - list; )
+    //title ( string || null )
+
+    const data = fs.readFileSync('datadb.json');
+    // const data = fs.readFileSync('data.json', 'utf8');
+    // let data = fs.readFileSync('data.json');
+    // data = data.toString();
+    let tasks = JSON.parse(data);
+
 };
 
 handleCommand(command);
